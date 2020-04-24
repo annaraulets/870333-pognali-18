d915d415a5d5654ec63b1e88ddbd8b50b3f0b774
annaraulets <raulets2@gmail.com>
2020-04-23 20:42:43 -0700

    Final version

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var svgstore = require("gulp-svgstore");
+var rename = require("gulp-rename");
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -34,4 +37,35 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("sprite", function() {
+  return gulp.src("source/img/*.svg")
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("clean", function() {
+  return del("build")
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "source/fonts/*",
+    "source/img/*",
+    "source/js/*",
+    "source/*.html",
+    "source/css/*.css"
+    ], {
+      base: "source"
+    })
+  .pipe(gulp.dest("build"));
+});
+
+
+gulp.task("start", gulp.series("sprite", "css", "server"));
+gulp.task("build", gulp.series("clean", "sprite", "css", "copy"));
+
+
+
